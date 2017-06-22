3849d7c889044c75ddcb3a73e0c41c86f249ee4c
Jason Mirra <mirraj2@gmail.com>
2017-06-22 16:44:52 -0700

    collission detection

--- src/glitter/client/js/input.js
+++ src/glitter/client/js/input.js
@@ -4,26 +4,71 @@
 
 function Input() {
   this.keys = {};
+  this.rect = new PIXI.Rectangle();
 }
 
 Input.prototype.update = function(t) {
+  if (window.me == null) {
+    return;
+  }
+
   var keys = this.keys;
 
   var speed = 3;
   var distance = speed * TILE_SIZE * t / 1000;
 
+  var dx = 0, dy = 0;
+
   if (keys['w']) {
-    me.setY(me.y - distance);
+    dy -= distance;
   }
   if (keys['a']) {
-    me.setX(me.x - distance);
+    dx -= distance;
   }
   if (keys['s']) {
-    me.setY(me.y + distance);
+    dy += distance;
   }
   if (keys['d']) {
-    me.setX(me.x + distance);
+    dx += distance;
   }
+
+  if (dx != 0) {
+    this.move(me, dx, 0);
+  }
+  if (dy != 0) {
+    this.move(me, 0, dy);
+  }
+}
+
+Input.prototype.move = function(player, dx, dy) {
+  var rect = this.rect;
+  rect.x = player.x + dx + player.hitbox.x;
+  rect.y = player.y + dy + player.hitbox.y;
+  rect.width = player.hitbox.width;
+  rect.height = player.hitbox.height;
+
+  if (this.intersectsBadTerrain(rect)) {
+    return;
+  }
+
+  player.setX(player.x + dx);
+  player.setY(player.y + dy);
+}
+
+Input.prototype.intersectsBadTerrain = function(rect) {
+  var minI = Math.floor(rect.x / TILE_SIZE);
+  var minJ = Math.floor(rect.y / TILE_SIZE);
+  var maxI = Math.floor((rect.x + rect.width) / TILE_SIZE);
+  var maxJ = Math.floor((rect.y + rect.height) / TILE_SIZE);
+
+  for (var i = minI; i <= maxI; i++) {
+    for (var j = minJ; j <= maxJ; j++) {
+      if (!world.terrain.isWalkable(i, j)) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 Input.prototype.listen = function() {
--- src/glitter/client/js/terrain.js
+++ src/glitter/client/js/terrain.js
@@ -1,11 +1,21 @@
 function Terrain(json) {
   this.tiles = json.tiles;
+  this.width = this.tiles.length;
+  this.height = this.tiles[0].length;
 }
 
 Terrain.prototype.getPixelWidth = function() {
-  return this.tiles.length * TILE_SIZE;
+  return this.width * TILE_SIZE;
 }
 
 Terrain.prototype.getPixelHeight = function() {
-  return this.tiles[0].length * TILE_SIZE;
+  return this.height * TILE_SIZE;
+}
+
+Terrain.prototype.isWalkable = function(i, j) {
+  if (i < 0 || j < 0 || i >= this.width || j >= this.height) {
+    return false;
+  }
+  var tile = this.tiles[i][j];
+  return tile == 1 || tile == 2;
 }
