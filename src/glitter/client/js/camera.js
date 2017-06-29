3d9a3f538c3c6a1ccf3449eeff31211d228359a1
Mike Reinstein <reinstein.mike@gmail.com>
2017-06-29 16:26:28 -0700

    smoothly move camera to player via lerp
    expirimenting with browserify/bundling
    
    bundle.js is no longer inlined
    
    added bundle.js to .gitignore

--- src/glitter/client/js/camera.js
+++ src/glitter/client/js/camera.js
@@ -1,48 +1,21 @@
-function Camera() {
+function interpolate (from, to, percent) {
+  percent = Math.min(percent, 1)
+  return from + percent * (to - from)
 }
 
+module.exports = function camera (options = {}) {
+  let update = function (millis) {
+    const w = $(window).width()
+    const h = $(window).height()
+    const me = window.me
+    const world = window.world
 
-function clamp(val, min, max) {
-  if(val >= max) return max;
-
-  if(val <= min) return min;
-
-  return val;
-}
-
-
-function scalarLerp(p, n, t) {
-  let _t = clamp(Number(t), 0.0, 1.0);
-  return p + _t * (n - p);
-}
-
-
-function pointLerp(p, n, t) {
-  p.x = scalarLerp(p.x, n.x, t);
-  p.y = scalarLerp(p.y, n.y, t);
-}
-
-
-Camera.prototype.update = function(t) {
-  var w = $(window).width();
-  var h = $(window).height();
-  // if (world.terrain) {
-  // world.container.x = Math.round((w - world.terrain.getPixelWidth()) / 2);
-  // world.container.y = Math.round((h - world.terrain.getPixelHeight()) / 2);
-  // }
-
-  if (window.me) {
-    var newCharacterPosition = {
-      x: Math.round(w / 2 - (Math.round(me.x) + me.width / 2)),
-      y: Math.round(h / 2 - (Math.round(me.y) + me.height / 2))
+    if (me) {
+      // move 10% of the way torwards the character's current position
+      world.container.x = Math.round(interpolate(world.container.x, w / 2 - (me.x + me.width / 2), millis / 200))
+      world.container.y = Math.round(interpolate(world.container.y, h / 2 - (me.y + me.height / 2), millis / 200))
     }
-
-    // move 10% of the way torwards the character's current position
-    pointLerp(world.container, newCharacterPosition, 0.1);
-
-    //world.container.x = 100;
-    //world.container.y = 100;
-    //world.container.scale.x = .1;
-    //world.container.scale.y = .1;
   }
+
+  return Object.freeze({ update })
 }
--- src/glitter/client/js/glitter.js
+++ src/glitter/client/js/glitter.js
@@ -1,31 +1,35 @@
-window.Tile.SIZE = 48;
+
+const Camera = require('./camera')
+
+
+window.Tile.SIZE = 48
 
 function Glitter() {
-  this.initGame();
+  this.initGame()
 }
 
 Glitter.prototype.initGame = function() {
-  window.canvas = new Canvas();
-  window.world = new World();
-  window.camera = new Camera();
-  window.input = new Input();
-  window.minimap = new MiniMap();
-  window.quickbar = new Quickbar();
-  
-  input.listen();
+  window.canvas = new Canvas()
+  window.world = new World()
+  window.camera = new Camera()
+  window.input = new Input()
+  window.minimap = new MiniMap()
+  window.quickbar = new Quickbar()
 
-  var loop = new GameLoop(canvas);
-  loop.start();
+  input.listen()
+
+  var loop = new GameLoop(canvas)
+  loop.start()
 
   PIXI.loader.add("tiles.png").add("wizard.png").load(function() {
-    console.log("done loading sprites.");
+    console.log("done loading sprites.")
 
-    window.network = new Network("$$(websocketIP)", $$(websocketPort));
-  });
+    window.network = new Network(window.websocketIP, window.websocketPort);
+  })
 }
 
 function main() {
-  new Glitter();
+  new Glitter()
 }
 
-$(main);
+$(main)
